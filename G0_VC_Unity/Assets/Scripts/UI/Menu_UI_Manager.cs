a74656526f170d3b159a83e7c6455e7351502231
MapleSyrupy <noel_yap@hotmail.com>
2025-02-02 14:41:21 -0800

    Upgraded the Menu

--- G0_VC_Unity/Assets/Scripts/UI/Menu_UI_Manager.cs
+++ G0_VC_Unity/Assets/Scripts/UI/Menu_UI_Manager.cs
@@ -11,37 +11,70 @@ using System.Threading.Tasks;
 using Steamworks.Data;
 using System;
 using UnityEngine.SceneManagement;
+using System.Linq;
+using JetBrains.Annotations;
 
 
-public class Menu_UI_Manager:UI_Manager
+public class Menu_UI_Manager : UI_Manager
 {
     [SerializeField] public Button CreateGameButton;
-    [SerializeField] public NetworkManager NetworkManager;
+    [SerializeField] public Button JoinGameButton;
+    [HideInInspector] public int MainKey;
 
-    new private static Menu_UI_Manager instance { get; set; }
 
-    void Start()
+
+
+
+    public static Menu_UI_Manager Menu_instance { get; set; }
+
+    //instead of adding listeners, just reference function through inspector
+
+    //declare a list of Screen_UIs, but then in runtime turn the listttttttt into a dictionary
+
+    //use dictionary to pass in differnet key values to switch to, try and make this a parameter in a function instead of hardcoding numbers
+
+    //async void Start ()
+    //{
+    //    using (var list = new Steamworks.ServerList.Internet())
+    //    {
+    //        list.AddFilter("map", "de_dust");
+    //        await list.RunQueryAsync();
+
+    //        foreach (var server in list.Responsive)
+    //        {
+    //            Debug.Log($"{server.Address} {server.Name}");
+    //        }
+    //    }
+    //}
+
+
+  
+
+    //void Update()
+    //{
+        
+    //}
+
+
+    public void onClicky()
     {
-        NetworkManager = GetComponent<NetworkManager>();
-
-        CreateGameButton.onClick.AddListener(onClicky);
-
+        //NetworkManager.Singleton.StartHost();
+        SteamLobbyManager.instance.CreateLobby();
+       
     }
-
-    void onClicky()
+    //public void onClicky2()
+    //{
+    //    Debug.Log("mimi"); 
+    //}
+    public void ConstructMenuSingleton()
     {
-        NetworkManager.Singleton.StartHost();
-
-    }
-    public override void ConstructSingleton()
-    {
-        if (instance != null && instance != this)
+        if (Menu_instance != null && Menu_instance != this)
         {
-            Destroy(this);
+            Destroy(this); 
         }
         else
         {
-            instance = this;
+            Menu_instance = this;
         }
     }
 
--- G0_VC_Unity/Assets/Scripts/UI/UI_Manager.cs
+++ G0_VC_Unity/Assets/Scripts/UI/UI_Manager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,13 +8,29 @@ using UnityEngine.UI;
 public class UI_Manager : MonoBehaviour
 {
     public static UI_Manager instance { get; set; }
-
+    Dictionary<int, Screen_UI> ScreenDict = new Dictionary<int, Screen_UI>();
+    public int currentScreen; 
     private void Awake()
     {
         ConstructSingleton();
+        List<Screen_UI> ScreenList = Resources.FindObjectsOfTypeAll<Screen_UI>().ToList();
+        foreach (Screen_UI screen in ScreenList)
+        {
+            ScreenDict.Add(screen.Key, screen);
+            if (screen.Key==0)//Default Screen is 0, that's why its 0.
+            {
+                screen.EnableScreen();
+                currentScreen = 0;
 
+            }
+        }
     }
-
+    public void SwitchScreens(int newScreen)
+    {
+        ScreenDict[currentScreen].DisableScreen();
+        ScreenDict[newScreen].EnableScreen();
+        currentScreen = newScreen;
+    }
     public virtual void ConstructSingleton()
     {
         if (instance != null && instance != this)
